89400c89c2949f08360ee0765d9201e5d4092f62
Sachin Jadhav <sachinjadhav@Sachins-MacBook-Pro.local>
2023-03-25 16:49:07 +0530

    Refactor server.js and add animation with loading screen

--- js/index.js
+++ js/index.js
@@ -12,59 +12,92 @@ const STEP_INFO = [
 ];
 const LEFT_INFO = [1, 2, 6, 7, 8];
 const RIGHT_INFO = [3, 4, 5];
-const INFO_MARGIN = "175px";
+const INFO_MARGIN = "100px";
 const UNSET = "unset";
 
 let position = 0;
 let intervalVar = null;
 
+// Get all the elements
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 const bg = document.getElementsByClassName("bg")[0];
+const liItems = document.getElementsByTagName("li");
 const infoDiv = document.getElementById("information");
+const stepDetails = document.getElementById("step-details");
+const loadingScreen = document.getElementsByClassName("loading-screen")[0];
+const contentToShow = document.getElementsByClassName("content-to-show")[0];
 
-function animateToStep(pos) {
+// IIFE to call tiemout to hide loading screen
+(function init() {
+  const timeoutVar = setTimeout(() => {
+    (loadingScreen.style.visibility = "hidden"),
+      (contentToShow.style.visibility = "visible");
+  }, 3000);
+})();
+
+function animateToStep(pos, previousPos, currentPos) {
   bg.style.backgroundPosition = pos + "% 0%";
 
-  infoDiv.innerHTML = position >= 0 ? STEP_INFO[position - 1] : "";
+  infoDiv.innerHTML = position >= 1 ? STEP_INFO[position - 1] : "";
   const isRight = RIGHT_INFO.includes(position);
 
+  // Show step info to the right or left
   infoDiv.style.right = isRight ? INFO_MARGIN : UNSET;
   infoDiv.style.left = isRight ? UNSET : INFO_MARGIN;
+
+  // Show step position info
+  stepDetails.innerHTML =
+    position >= 1
+      ? `Step ${position} out of ${TOTAL_STEP} on the path to digital enlightenment`
+      : "";
+
+  liItems[currentPos].style.backgroundColor = "white";
+  liItems[previousPos].style.backgroundColor = "transparent";
 }
 
+// Register listener for next button
 nextButton.addEventListener("click", () => {
   if (position < TOTAL_STEP) {
     position += 1;
     let count = 0;
-
     intervalVar = setInterval(() => {
       if (count === DEFAULT_STEP_MOVE) {
         clearInterval(intervalVar);
       } else {
-        animateToStep(position * DEFAULT_STEP_MOVE + count);
+        animateToStep(
+          position * DEFAULT_STEP_MOVE + count,
+          position - 1,
+          position
+        );
         count++;
       }
     }, 50);
   }
 });
 
+// Register listener for prev button
 prevButton.addEventListener("click", () => {
   if (position > 0) {
+    position -= 1;
     let count = 0;
     intervalVar = setInterval(async () => {
       if (count === DEFAULT_STEP_MOVE) {
         clearInterval(intervalVar);
       } else {
-        animateToStep(DEFAULT_STEP_MOVE * (position + 1) - count);
+        animateToStep(
+          DEFAULT_STEP_MOVE * (position + 1) - count,
+          position + 1,
+          position
+        );
         count++;
       }
     }, 50);
-    position -= 1;
   }
 });
 
+// Clear interval on leaving the page
 window.addEventListener("beforeunload", function (e) {
   if (intervalVar) {
     clearInterval(intervalVar);
--- server.js
+++ server.js
@@ -2,40 +2,75 @@ var fs = require("fs");
 var http = require("http");
 var url = require("url");
 var path = require("path");
-var util = require("util");
 
 var dir = path.dirname(fs.realpathSync(__filename));
+
+var mime = {
+  html: "text/html",
+  txt: "text/plain",
+  css: "text/css",
+  gif: "image/gif",
+  jpg: "image/jpeg",
+  png: "image/png",
+  svg: "image/svg+xml",
+  js: "application/javascript",
+};
+
+function onNotFound(res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.write("404 Not Found\n");
+  res.end();
+}
+
+function onDefaultPath(res) {
+  res.writeHead(200, { "Content-Type": mime.html });
+  fs.createReadStream(dir + "/index.html").pipe(res);
+  return;
+}
+
+function onJsCssPath(res, pathname, m) {
+  var filename = dir + pathname;
+  var stats = fs.existsSync(filename) && fs.statSync(filename);
+  if (stats && stats.isFile()) {
+    res.writeHead(200, {
+      "Content-Type": m[1] === "js" ? mime.js : mime.css,
+    });
+    fs.createReadStream(filename).pipe(res);
+    return;
+  }
+}
+
+function onImages(res, pathname, m) {
+  var filename = dir + pathname;
+  var ext = path.extname(filename).slice(1);
+  const contentType =
+    ext === "svg"
+      ? {
+          "Content-Type": mime.svg,
+        }
+      : {}; // {} will handle the case for all the image types internally
+  var stats = fs.existsSync(filename) && fs.statSync(filename);
+  if (stats && stats.isFile()) {
+    res.writeHead(200, {
+      ...contentType,
+    });
+    fs.createReadStream(filename).pipe(res);
+    return;
+  }
+}
 
 http
   .createServer(function (req, res) {
     var pathname = url.parse(req.url).pathname;
     var m;
     if (pathname == "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      fs.createReadStream(dir + "/index.html").pipe(res);
-      return;
+      onDefaultPath(res);
     } else if ((m = pathname.match(/^\/(js|css)\//))) {
-      var filename = dir + pathname;
-      var stats = fs.existsSync(filename) && fs.statSync(filename);
-      if (stats && stats.isFile()) {
-        res.writeHead(200, {
-          "Content-Type": m[1] === "js" ? "application/javascript" : "text/css",
-        });
-        fs.createReadStream(filename).pipe(res);
-        return;
-      }
+      onJsCssPath(res, pathname, m);
     } else if ((m = pathname.match(/^\/(images)\//))) {
-      var filename = dir + pathname;
-      var stats = fs.existsSync(filename) && fs.statSync(filename);
-      if (stats && stats.isFile()) {
-        res.writeHead(200);
-        fs.createReadStream(filename).pipe(res);
-        return;
-      }
+      onImages(res, pathname, m);
     } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.write("404 Not Found\n");
-      res.end();
+      onNotFound(res);
     }
   })
   .listen(8000, "localhost");
